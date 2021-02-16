e143a8faad2b1ccbdaf16dd227a3434cf6f809e8
Sagitario2.5 <adanielsanchezgarcia@gmail.com>
2021-02-16 01:20:18 +0100

    Added a logger and print function

--- main.py
+++ main.py
@@ -15,56 +15,44 @@ def mkdir (newdirs):#Creation of new dirs
 def relativePath (relative = ""):#Relative path
 	return os.path.join (os.path.dirname (__file__), relative)
 
+def output (message, display = False, logger = None, addLog = "\n"):
+	if display:
+		print (message)
+	if logger != None:
+		logger.write (message + addLog)
+
 def backupPath (fromPath, toPath, logger = None, display = True):#Everythong related to the creation of the backup
 	if not os.path.exists (toPath):
 		mkdir (toPath)
 	name = toPath.split ('\\')[-1]
 
 	#Read the data from fromPath
-	if display:
-		print (f"\tOpening {name}")
-	if logger != None:
-		logger.write (f"\n\t-{name}\n")
+	output (f"\tOpening {name}", display = display, logger = logger)
 	with gestor.Gestor (fromPath, toPath) as gest: 
-		if display:
-			print ("\t\tComparing...")
+		output ("\t\tComparing...", display = display)
 		if not gest.compare ():#Compare if new version is needed
 			#New version
-			if display:
-				print ("\t\tNew version needed. Copying...")
-			if logger != None:
-				logger.write (f"\t\tcreating new version\t{time.ctime ()}\n")
+			output ("\t\tNew version needed. Copying...", display = display, logger = logger)
 			gest.move (newDir = copyTime)
-			if logger != None:
-				logger.write (f"\t\tend of new version\t{time.ctime ()}\n")
+			output (f"\t\tend of new version\t{time.ctime ()}\n", logger = logger)
 		else:
 			#No need
-			if display:
-				print ("\t\tLast version is updated. No need to copy.")
-			if logger != None:
-				logger.write (f"\t\tno need to copy\n")
+			output ("\t\tLast version is updated. No need to copy.", display = display, logger = logger)
 
-	print (f"\t{name} ended.\n")
+	output (f"\t{name} ended.\n", display = display, logger = logger)
 
 def clearBackup (path, gulagPath = None, logger = None, display = True):
 	name = path.split ('\\')[-1]
-	if display:
-		print (f"\tOpening {name}")
-		print (f"\t\tAnalising...")
-	if logger != None:
-		logger.write (f"\t-{name}\n")
-		logger.write (f"\t\tAnalising...\n")
+	output (f"\tOpening {name}", display = display, logger = logger)
+	output (f"\t\tAnalising...", display = display, logger = logger)
 	gest = gestor.versionGestor (path)
 	tokeep = gest.clean ()
-	if display:
-		print (f"\t\tDeleting {tokeep.count (False)} versions...")
-	if logger != None:
-		logger.write (f"\t\tDeleting {tokeep.count (False)} versions...:")
-		logger.write (f"{str (gest.getunwanted ())}\n")
+	output (f"\t\tDeleting {tokeep.count (False)} versions...", display = display, logger = logger)
+	output (f"{str (gest.getunwanted ())}\n", logger = logger)
 	mkdir (gulagPath)
 	gest.delversions (gulagPath)
 
-	print ("\tCleaning ended\n")
+	output ("\tCleaning ended\n", display = display)
 
 def main ():
 	#Reads config.txt and asigns its data
@@ -85,18 +73,16 @@ def main ():
 	copiesInfo = copies.contents
 
 	#Actual copying
-	logfile.write ("\nStarting backup creation")
-	print ("Starting backup creation")
+	output ("Starting backup creation", display = True, logger = logfile)
 	for i in copiesInfo:
 		#Get fromPath and toPath from the copy file
 		origin = i
 		backup = os.path.join (dirsPath, copiesInfo [i])
 		backupPath (origin, backup, logfile)
-	logfile.write (f"Ending backupcreation {time.ctime ()}")
+	output (f"Ending backup creation {time.ctime ()}", logger = logfile)
 
 	#Deletion of unnecesary versions
-	print ("\n\nStarting version cleaner")
-	logfile.write ("\nStarting version cleaner\n")
+	output ("\nStarting version cleaner", display = True, logger = logfile)
 	for i in copiesInfo:
 		path = copiesInfo [i]
 		clearBackup (path = os.path.join (dirsPath, path), gulagPath = os.path.join (dirsPath, "- gulag", path), logger = logfile)
